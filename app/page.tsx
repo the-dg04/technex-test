0f52b99b5979e56588a62322bea8fb8370af9ae7
the-dg04 <devgup04@gmail.com>
2024-12-22 16:27:03 +0530

    redirected to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(()=>{
+    router.push("/home")
+  })
   return (
     <>E</>
   );
